c1fcbbdf7f4fe5be91662a130a043085ae06afec
Xavier Gillmann <xgi0101@gmail.com>
2019-05-08 11:23:07 +0200

    initial empty

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ClaimMenu from "./components/ClaimMenu";
+
+class ClaimModule extends Component {
+  static menu = null;
+  static routes = [];
+}
+
+export default ClaimModule;
