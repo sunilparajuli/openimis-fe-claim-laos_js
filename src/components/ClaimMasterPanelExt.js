223b2497e16178dedab113a3c0d8c20a2e554451
wzglinieckisoldevelo <98958634+wzglinieckisoldevelo@users.noreply.github.com>
2023-08-01 16:30:03 +0200

    ONI-64: Policy active/inactive status indicator. (#113)
    * ONI-64: Basic version of label change.
    
    * ONI-64: Proper date conversion for policy status.
    
    * ONI-64: Code review fix.

--- src/components/ClaimMasterPanelExt.js
+++ src/components/ClaimMasterPanelExt.js
@@ -4,12 +4,24 @@ import { bindActionCreators } from "redux";
 import { withTheme, withStyles } from "@material-ui/core/styles";
 import { Grid, Typography, Divider } from "@material-ui/core";
 import { PublishedComponent, FormattedMessage, ProgressOrError, TextInput } from "@openimis/fe-core";
-import {clearLastClaimAt, fetchLastClaimAt} from "../actions";
+import { clearLastClaimAt, fetchLastClaimAt } from "../actions";
+import { getTimeDifferenceInDaysFromToday } from "@openimis/fe-core";
 
 const styles = (theme) => ({
   tableHeader: theme.table.header,
   item: theme.paper.item,
+  activeLabel: {
+    padding: 10,
+  },
+  inactiveLabel: {
+    padding: 10,
+    color: "#e20606",
+  },
 });
+
+const ACTIVE_LABEL = "ClaimMasterPanelExt.InsureePolicyEligibilitySummaryActive.header";
+const INACTIVE_LABEL = "ClaimMasterPanelExt.InsureePolicyEligibilitySummaryInactive.header";
+const DEFAULT_LABEL =  "ClaimMasterPanelExt.InsureePolicyEligibilitySummary.header";
 
 class ClaimMasterPanelExt extends Component {
   componentDidMount() {
@@ -40,13 +52,20 @@ class ClaimMasterPanelExt extends Component {
     this.props.clearLastClaimAt();
   }
 
+  getPolicyStatusLabel(timeDelta) { return timeDelta >= 0 ? ACTIVE_LABEL : INACTIVE_LABEL; };
+
+  getPolicyStatusLabelStyle(timeDelta, classes) { return timeDelta >= 0 ? classes.activeLabel : classes.inactiveLabel; }
+
   render() {
     const { classes, claim, fetchingLastClaimAt, errorLastClaimAt, fetchedLastClaimAt, lastClaimAt } = this.props;
+    const timeDelta = getTimeDifferenceInDaysFromToday(this.props.currentPolicy ? this.props.currentPolicy?.[0]?.expiryDate : null);
+    const policyStatusLabel = this.props.currentPolicy ? this.getPolicyStatusLabel(timeDelta) : DEFAULT_LABEL;
+    const policyStatusLabelStyle = this.props.currentPolicy ? this.getPolicyStatusLabelStyle(timeDelta, classes) : classes.item;
     return (
       <Grid container>
         <Grid item xs={6} className={classes.item}>
-          <Typography className={classes.tableTitle}>
-            <FormattedMessage module="claim" id="ClaimMasterPanelExt.InsureePolicyEligibilitySummary.header" />
+          <Typography className={policyStatusLabelStyle}>
+            <FormattedMessage module="claim" id={policyStatusLabel} />
           </Typography>
           <Divider />
         </Grid>
@@ -111,6 +130,7 @@ const mapStateToProps = (state) => ({
   fetchedLastClaimAt: state.claim.fetchedLastClaimAt,
   lastClaimAt: state.claim.lastClaimAt,
   errorLastClaimAt: state.claim.errorLastClaimAt,
+  currentPolicy: state.policy.policies,
 });
 
 const mapDispatchToProps = (dispatch) => {
